941e60999fad3145a62a6b3b5df57c8740e532a9
fredplusplus@gmail.com <fredplusplus@gmail.com@dd6ad4a0-136c-11df-b355-3d0bf613b719>
2010-02-08 00:34:58 +0000

    started datamanger

---
+++ src/citybus/datamanager/DataManager.java
@@ -0,0 +1,12 @@
+package citybus.datamanager;
+
+/**
+ * Data Manager. This is central data system for bus schedules and location
+ * information
+ * 
+ * @author zhang42
+ * 
+ */
+public class DataManager {
+
+}
