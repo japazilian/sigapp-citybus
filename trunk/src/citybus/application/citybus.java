da42f786713ba555bdb50f1f8e98a6ed7ee72b0b
rharagutchi@gmail.com <rharagutchi@gmail.com@dd6ad4a0-136c-11df-b355-3d0bf613b719>
2010-02-11 01:17:28 +0000

    AR is working, don't forget to install wikitude to make it work. Also if eclipse says you don't have the jar, it's on the website, http://sites.google.com/site/purduesigapp/projects/citybus/citybus-files

--- trunk/src/citybus/application/citybus.java
+++ trunk/src/citybus/application/citybus.java
@@ -8,130 +8,61 @@ import org.openintents.intents.WikitudeARIntent;
 import org.openintents.intents.WikitudePOI;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.ActivityNotFoundException;
-import android.content.DialogInterface;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class citybus extends Activity {
-	
-	/** the callback-intent after pressing any buttons */
-    private static final String CALLBACK_INTENT = "wikitudeapi.mycallbackactivity";
-    
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        //Test AR button setup, TODO change once we have real GUI
         Button b = (Button) findViewById(R.id.btn_startar);
         b.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                citybus.this.startARViewWithIcons();
+            	startAR();
             }
         });
     }
-    
     /**
-     * does the same as the basic AR view above, but adds custom icons to the POIs
+     * startAR opens up the activity for the augmented reality 
      */
-    void startARViewWithIcons() {
-
-        // Create the basic intent
-        WikitudeARIntent intent = prepareIntent();
-
-        // Optionally add a title
-        intent.addTitleText("AR app with custom icons");
-        intent.setPrintMarkerSubText(false);
-        
-        // Optionally: Add icons
-        //addIcons(intent);
-
-        // And launch the intent
+    public void startAR() {
+    	WikitudeARIntent intent = new WikitudeARIntent(this.getApplication(), 
+    			"F606E0CFD83ACC9801914AFF3F7B159C", "Purdue ACM SigApp"); //create new intent, needs a key that you get after registering with them
+        intent.addPOIs(createPOIList()); //Adds the array list to the intent
         try {
-            intent.startIntent(this);
-        } catch (ActivityNotFoundException e) {
-            AbstractWikitudeARIntent.handleWikitudeNotFound(this);
+        	intent.startIntent(this); //try starting the new intent (could fail if they don't have wikitude, which is dumb...)
+        }
+        catch(ActivityNotFoundException e) {
+        	 AbstractWikitudeARIntent.handleWikitudeNotFound(this);//tells them to install wikitude
         }
     }
     /**
-     * prepares a Wikitude AR Intent (e.g. adds the POIs to the view)
+     * Method createPOIList creates the list of points of interest
      * 
-     * @return the prepared intent
+     * TODO-I am a bit worried about dynamic refreshing of information, like changing the time of the next bus
+     * @return 
+     * an array list of the points of interest to be displayed to the user in the AR
      */
-    private WikitudeARIntent prepareIntent() {
-        // create the intent
-        WikitudeARIntent intent = new WikitudeARIntent(this.getApplication(), null, null);
-        // add the POIs
-        this.addPois(intent);
-        // add one menu item
-        intent.setMenuItem1("My menu item", citybus.CALLBACK_INTENT);
-        intent.setPrintMarkerSubText(true);
-        return intent;
-    }
-    
-    /**
-     * adds hard-coded POIs to the intent
-     * 
-     * @param intent
-     *            the intent
-     */
-    private void addPois(WikitudeARIntent intent) {
-        WikitudePOI poi1 = new WikitudePOI(35.683333, 139.766667, 36, "Tokyo", "Tokyo is the capital of Japan.");
+    public List<WikitudePOI> createPOIList() {
+    	List<WikitudePOI> pois = new ArrayList<WikitudePOI>();
+    	WikitudePOI poi1 = new WikitudePOI(35.683333, 139.766667, 36, "Tokyo", "Tokyo is the capital of Japan.");
         poi1.setLink("http://www.tourism.metro.tokyo.jp/");
-        poi1.setDetailAction(citybus.CALLBACK_INTENT);
         WikitudePOI poi2 = new WikitudePOI(41.9, 12.5, 14, "Rome",
                 "Rome is the capital of Italy and the country's largest and most populous city, with over 2.7 million residents.");
-        poi2.setDetailAction(citybus.CALLBACK_INTENT);
         WikitudePOI poi3 = new WikitudePOI(40.716667, -74, 1, "New York",
                 "New York is the most populous city in the United States, and the center of the New York metropolitan area.");
-        poi3.setDetailAction(citybus.CALLBACK_INTENT);
         WikitudePOI poi4 = new WikitudePOI(48.208333, 16.373056, 220, "Vienna",
                 "Vienna is the capital of the Republic of Austria.");
-        poi4.setDetailAction(citybus.CALLBACK_INTENT);
-        List<WikitudePOI> pois = new ArrayList<WikitudePOI>();
-
         pois.add(poi1);
         pois.add(poi2);
         pois.add(poi3);
         pois.add(poi4);
-        intent.addPOIs(pois);
-
-        ((citybusApplication) this.getApplication()).setPois(pois);
+        return pois;
     }
-
-    /**
-     * helper-method to add icons to the intent.
-     * 
-     * @param intent
-     *            the intent
-     */
-    private void addIcons(WikitudeARIntent intent) {
-        ArrayList<WikitudePOI> pois = intent.getPOIs();
-
-        Resources res = getResources();
-        //pois.get(0).setIconresource(res.getResourceName(R.drawable.flag_japan));
-        //pois.get(1).setIconresource(res.getResourceName(R.drawable.flag_italy));
-        //pois.get(2).setIconresource(res.getResourceName(R.drawable.flag_usa));
-        //pois.get(3).setIconresource(res.getResourceName(R.drawable.flag_austria));
-        // to use this, make sure you have the file present on the sdcard
-        // pois.get(3).setIconuri("content://com.IconCP/sdcard/flag_austria.png");
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected Dialog onCreateDialog(int dialogId) {
-        //String message = this.getString(R.string.modelfile_not_found, modelFileName);
-        return new AlertDialog.Builder(this).setTitle("File not found").setMessage("model not found")
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        // do nothing, just dismiss the dialog
-                    }
-                }).create();
-    }
+   
 }
