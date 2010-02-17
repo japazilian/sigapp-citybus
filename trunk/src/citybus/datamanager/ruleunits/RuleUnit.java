78141138c26c252c47570a956f5b1f11f58f0229
fredplusplus@gmail.com <fredplusplus@gmail.com@dd6ad4a0-136c-11df-b355-3d0bf613b719>
2010-02-17 21:54:38 +0000

    continue work on bus rule data

--- trunk/src/citybus/datamanager/ruleunits/RuleUnit.java
+++ trunk/src/citybus/datamanager/ruleunits/RuleUnit.java
@@ -1,14 +1,39 @@
 package citybus.datamanager.ruleunits;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Bus Schedule Rules
+ * 
+ * @author zhang42
+ * 
+ */
 public class RuleUnit {
 
-	public int[] stopGap;
-	public int[][] emptyBlocks;
+	// time interval in minutes between each stop
+	protected int[] stopGap;
+	// blocks that should be empty, row
+	protected ArrayList<Block> emptyBlocks;
+	// first bus stop arrive time (always 2nd row, 1st column if special !=null,
+	// else 1st row,col)
+	protected Date startTime;
+	// last bus's first stop arrive time (always last row,1st column)
+	protected Date endTime;
+	// special times, always 1st row or null
+	protected Date[] specialTimes;
 
 	public RuleUnit() {
 		stopGap = null;
 		emptyBlocks = null;
 	}
-	
-	
+
+	protected class Block {
+		public int row, column;
+
+		public Block(int r, int c) {
+			row = r;
+			column = c;
+		}
+	}
 }
