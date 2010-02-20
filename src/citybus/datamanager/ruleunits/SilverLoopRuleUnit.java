a09299b0c0aed2daa61feabc3fa69c905b4abbef
fredplusplus@gmail.com <fredplusplus@gmail.com@dd6ad4a0-136c-11df-b355-3d0bf613b719>
2010-02-20 21:38:27 +0000

    fixed some bugs, added silver loop info

--- src/citybus/datamanager/ruleunits/SilverLoopRuleUnit.java
+++ src/citybus/datamanager/ruleunits/SilverLoopRuleUnit.java
@@ -1,6 +1,17 @@
 package citybus.datamanager.ruleunits;
 
+import citybus.datamanager.DBConstants;
+import citybus.datamanager.Time;
 
 public class SilverLoopRuleUnit extends RuleUnit {
 
+	public SilverLoopRuleUnit() {
+		routineIndex = DBConstants.RoutineIndex[1];
+		stopGap = new int[] { 2, 2, 10 };
+		busGap = new int[][] { { 5, 5, 10 }, { 3, 5, 10, 2 }, { 10 } };
+		busGapCutoffs = new Time[] { new Time(11, 27), new Time(17, 55) };
+		startTime = new Time(7, 5);
+		endTime = new Time(17, 55);
+		initFields();
+	}
 }
