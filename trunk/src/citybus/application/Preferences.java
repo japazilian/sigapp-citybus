f6d8f05afaa34756dd6cb33205bb4af9f75f4d3a
fredplusplus@gmail.com <fredplusplus@gmail.com@dd6ad4a0-136c-11df-b355-3d0bf613b719>
2010-03-15 17:09:18 +0000

    removed some redundant code. reorganized code format a little bit.

--- trunk/src/citybus/application/Preferences.java
+++ trunk/src/citybus/application/Preferences.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class Preferences extends PreferenceActivity {
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-super.onCreate(savedInstanceState);
-addPreferencesFromResource(R.xml.prefscreen);
-//Get the custom preference
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.prefscreen);
+		// Get the custom preference
 
-}
+	}
 }
