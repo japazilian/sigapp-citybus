fe34ff78067b151c8d67255892362d1ccf25fe3c
rharagutchi@gmail.com <rharagutchi@gmail.com@dd6ad4a0-136c-11df-b355-3d0bf613b719>
2010-03-15 15:08:23 +0000

    Made the presentation of stop information more elegant. It's taking a bit of time on the emulator and my old G1 phone, might need to find a way to improve searching.

--- trunk/src/citybus/application/busStopItemizedOverlay.java
+++ trunk/src/citybus/application/busStopItemizedOverlay.java
@@ -1,11 +1,19 @@
 package citybus.application;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import citybus.datamanager.BusNextComingInfo;
+import citybus.datamanager.DBConstants;
+import citybus.datamanager.DataManager;
+import citybus.datamanager.Time;
 
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.OverlayItem;
@@ -36,20 +44,103 @@ public class busStopItemizedOverlay extends ItemizedOverlay<OverlayItem> {
 	}
 	@Override
 	protected boolean onTap(int index) {
-		
 	  OverlayItem item = mOverlays.get(index);
-	 // Log.d("index: "+index+"item"+item.getTitle(), null);
+	  Calendar now = Calendar.getInstance();
+	  SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+	  String message = "";
+	  int id = -1;
+	  boolean firstPrint = true;
+	  
+	  ArrayList<BusNextComingInfo> info = DataManager.getNextComingBusInfoByLocation(//Date might be weird, check with Fan TODO
+				mContext, stopIdByName(item.getTitle()), new Time(now.get(Calendar.DAY_OF_WEEK), 
+						now.get(Calendar.HOUR_OF_DAY), now.get(Calendar.MINUTE)), 3);
+	  Log.d("citybus debug", now.get(Calendar.HOUR_OF_DAY)+":"+ now.get(Calendar.MINUTE));
+	  for (BusNextComingInfo i : info) {
+		  if(!checkPreferences(i.routeId, prefs))//check if they put it in the preferences
+			  continue;
+		  if(id != i.routeId) {
+			  if(firstPrint)
+				  firstPrint = false;
+			  else
+				  message += "\n";
+			  message += loopById(i.routeId) + ":\n";
+			  message += "\t[" + formatTime(i) + "]\n";
+			  id = i.routeId;
+		  }
+		  else {
+			  message += "\t[" + formatTime(i) + "]\n";
+		  }
+			  
+	  }
 	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
 	  dialog.setTitle(item.getTitle());
-	  dialog.setMessage(item.getSnippet());
+	  dialog.setMessage(message);
 	  dialog.setNegativeButton("Close", new DialogInterface.OnClickListener() {
           public void onClick(DialogInterface dialog, int id) {
               dialog.cancel();
          }
-     });
-	  //dialog.setTitle("test");
-	  //dialog.setMessage("now it works?");
+	  });
 	  dialog.show();
 	  return true;
+	}
+	private boolean checkPreferences(int i, SharedPreferences prefs) {		
+		switch(i) {
+		case 0: 
+			return prefs.getBoolean("goldloop", false);
+		case 1: 
+			return prefs.getBoolean("silverloop", false);
+		case 2: 
+			return prefs.getBoolean("blackloop", false);
+		case 3: 
+			return prefs.getBoolean("toweracres", false);
+		case 4: 
+			return prefs.getBoolean("bronzeloop", false);
+		case 5: 
+			return prefs.getBoolean("rossade", false);
+		case 6: 
+			return prefs.getBoolean("nightrider", false);
+		case 7: 
+			return prefs.getBoolean("southcampus", false);
+		}
+		return false;		
+	}
+	private String loopById(int i) {		
+		switch(i) {
+		case 0: 
+			return "(12) Gold Loop";
+		case 1: 
+			return "(13) Silver Loop";
+		case 2: 
+			return "(14) Black Loop";
+		case 3: 
+			return "(15) Tower Acres";
+		case 4: 
+			return "(16) Bronze Loop";
+		case 5: 
+			return "(17) Ross Ade";
+		case 6: 
+			return "(18) Night Rider";
+		case 7: 
+			return "(19) South Campus/AvTech";
+		}
+		return "";		
+	}
+	private int stopIdByName(String s) {
+		int i=0;
+		for(i=0; i<25; i++) {
+			if(s.equals(DBConstants.BusStopNames[i]))
+				return i;
+		}
+		return i;
+	}
+	private String formatTime(BusNextComingInfo i) {
+		String message = "";
+		if(i.geoTimeInfo.timeInfo.hour == 0)
+			  message += "12" + String.format(":%02d", i.geoTimeInfo.timeInfo.minute) + " am";
+		else if(i.geoTimeInfo.timeInfo.hour > 12) //pm
+			  message += String.format("%02d", i.geoTimeInfo.timeInfo.hour-12) + String.format(":%02d", i.geoTimeInfo.timeInfo.minute) + " pm";
+		else								//am
+			  message += String.format("%02d", i.geoTimeInfo.timeInfo.hour) + String.format(":%02d", i.geoTimeInfo.timeInfo.minute) + " am"; 
+		return message;
 	}
 }
